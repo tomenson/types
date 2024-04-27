6133715ff55142e31a6867bc9747e877e53de00e
tomenson <tomas.enarsson@outlook.com>
2024-04-27 22:27:57 +0200

    Add JSON functions

--- src/misc.ts
+++ src/misc.ts
@@ -1,5 +1,5 @@
 import { toError } from './error';
-import type { AnyFunction } from './types';
+import type { AnyFunction, JSONValue } from './types';
 
 /**
  * Does nothing at all.
@@ -7,6 +7,35 @@ import type { AnyFunction } from './types';
  */
 export function noop(): void {
   // Do nothing.
+}
+
+/**
+ * Wrapped JSON parse. Throws an error if the given value not is a string.
+ *
+ * @throws {SyntaxError | TypeError}
+ */
+export function parse<T>(
+  jsonStr: T,
+  reviver?: (key: string, value: unknown) => unknown,
+): T extends string ? JSONValue : never {
+  if (typeof jsonStr !== 'string') throw new TypeError(`${jsonStr === null ? 'null' : typeof jsonStr} is not a string`);
+  return JSON.parse(jsonStr, reviver);
+}
+
+/**
+ * Wrapped JSON stringify. Throws an error if the value not is allowed in JSON.
+ *
+ * @throws {TypeError}
+ */
+export function stringify<T>(
+  value: T,
+  replacer?: null | (number | string)[] | ((key: string, value: unknown) => unknown), space?: number | string,
+): T extends JSONValue ? string : never {
+  // @ts-ignore
+  const jsonStr = JSON.stringify(value, replacer, space) as string | undefined;
+  if (typeof jsonStr !== 'string') throw new TypeError(`${typeof value} cannot be stringified`);
+  // @ts-ignore
+  return jsonStr;
 }
 
 /**
