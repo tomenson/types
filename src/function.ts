6319dd7757b8808747a297297b5b2671b0f246ed
tomenson <tomas.enarsson@outlook.com>
2024-04-27 20:58:10 +0200

    Use any function type

--- src/function.ts
+++ src/function.ts
@@ -1,11 +1,12 @@
+import type { AnyFunction } from './types';
+
 /**
  * Returns the value if it's a function, otherwise throws an error.
  * Functional alternative to function casting.
  *
  * @throws {TypeError}
  */
-// eslint-disable-next-line @typescript-eslint/ban-types
-export function asFunction<T>(value: T, errMsg?: string): T extends Function ? T : never {
+export function asFunction<T>(value: T, errMsg?: string): T extends AnyFunction ? T : never {
   // @ts-ignore
   if (typeof value === 'function') return value;
   throw new TypeError(errMsg || `${value === null ? null : typeof value} is not a function`);
@@ -15,16 +16,14 @@ export function asFunction<T>(value: T, errMsg?: string): T extends Function ? T
  * Returns true if the value is a function.
  * Functional alternative to function type guard.
  */
-// eslint-disable-next-line @typescript-eslint/ban-types
-export function isFunction<T extends Function>(value: unknown): value is T {
+export function isFunction<T extends AnyFunction>(value: unknown): value is T {
   return typeof value === 'function';
 }
 
 /**
  * Converts the value to a function.
  */
-// eslint-disable-next-line @typescript-eslint/ban-types
-export function toFunction<T>(value: T): T extends Function ? T : () => T {
+export function toFunction<T>(value: T): T extends AnyFunction ? T : () => T {
   // @ts-ignore
   return (typeof value === 'function' ? value : () => value);
 }
